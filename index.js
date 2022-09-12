70e1830783da6d9ba678f1e96fa1d92755ff6de8
yudiz-devang <113346334+yudiz-devang@users.noreply.github.com>
2022-09-12 15:43:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function imgchange(){
+//	var flag;
+	var second = parseInt(document.getElementById("sInput").value);
+	// if(typeof timeinterval != 'undefined'){
+		// clearInterval(timeinterval)
+	// }
+	// if(flag == true){
+		// clearInterval(timeinterval);
+	// }
+	
+	// var second = parseInt(document.getElementById("sInput").value);
+		const url = "https://picsum.photos/200/300?random=";
+		let i = 0;
+		var timeinterval = setInterval(()=>{
+		document.getElementById('img').src = url + i;	
+		console.log(second)
+		i++;
+		},second*1000);
+}
 
